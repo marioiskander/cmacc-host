2dec70e6ea1cc2416e0558db7d7e5a5822504b45
HazardJ <jamesghazard@gmail.com>
2017-02-18 14:36:37 -0800

    Adding comments and referencing a Landing.md.

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 DEFINE('ROOT' ,dirname(__FILE__) ); // Root directory of site
 
-DEFINE('URLFORREPO', 'https://github.com/CommonAccord/Cmacc-Source');	// The repo home
+DEFINE('URLFORREPO', 'https://github.com/CommonAccord/cmacc-host');	// The repo home. Change to your own repo home. This links directly to your repo file. 
 
-DEFINE('LANDING_MD', 'S/About/Landing.md');	// The website home
+DEFINE('LANDING_MD', 'Landing.md');	// The website home
 
-DEFINE('SOURCE_TAB_MESSAGE', 'Source');	// Tab
+DEFINE('SOURCE_TAB_MESSAGE', 'Source');	// Tab  These tab texts can be modified for another language or whatever.
 
 DEFINE('EDIT_TAB_MESSAGE', 'Edit');	// Tab
 
@@ -26,7 +26,7 @@ ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . LIB_PATH . DI
 
 DEFINE('ASSETS_PATH', 'image');  // Create global variable to our css and js assets
 
-DEFINE('URLFORDOCSINREPO', URLFORREPO . "/blob/master/Doc/");	// Docs in the repo
+DEFINE('URLFORDOCSINREPO', URLFORREPO . "/blob/master/Doc/");	// Docs in the repo on Github.  Modify "master" if you are working on another branch.  
 
 DEFINE('TEXT_EDIT_WINDOW_SIZE', 'cols=120 rows=30'); //Sets the size of the text edit window in both edit.php and openedit.php
 
